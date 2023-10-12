3ef7a062685c388aa00a3496dc119f54d5c1f8fb
TongQuangHung102 <hungjob102@gmail.com>
2023-10-12 09:21:42 +0700

    video7, teample string

---
+++ video7/video7.js
@@ -0,0 +1,9 @@
+console.log('Hello world from HTML')
+let a = 5;
+let b = 10;
+console.log('Fifteen is ' + (a + b) + ' and\nnot ' + (2 * a + b) + '.');
+console.log('Fifteen is ' + a + b + ' and\nnot ' + (2 * a + b) + '.');
+
+//url: /hoidanit/5 - Teamplate strings
+console.log(`Hoi dan it chẹc strings: a = ${a} b = ${b} a + b = ${a + b}`)
+console.log(`/hoidanit/${a}`)
