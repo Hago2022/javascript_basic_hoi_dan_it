028e79b5e35485cbf1cb87510af7cd618f7cac3c
TongQuangHung102 <hungjob102@gmail.com>
2023-10-13 09:59:16 +0700

    video9, array

---
+++ video9/video9.js
@@ -0,0 +1,21 @@
+console.log('Hello world from HTML9')
+
+let a = {
+    name: 'Eric',
+    age: 25
+};
+
+console.log('check data object: ', a)
+console.log(`my name's : `, a.name)
+
+//arr = [item1, item2, item3]
+let b = ['MU', 'Chelsea', 'Liver'];
+console.log('typeof b: ', typeof b, b)
+console.log('first item: ', b[0], 'second item: ', b[1], 'third item: ', b[2])
+
+let family = [
+    {name: 'Eric', age: 25},
+    {name: 'Monica', age: 24}
+]
+
+console.log('my family: ', family)
