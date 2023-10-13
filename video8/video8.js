8dd806e8d5e051978eebfba0b4fbef6b9b339283
TongQuangHung102 <hungjob102@gmail.com>
2023-10-13 09:32:28 +0700

    video 8, object type

---
+++ video8/video8.js
@@ -0,0 +1,14 @@
+console.log('Hello world')
+
+//Key: value
+let obj = {
+    name: 'Eric', 
+    address: 'Ha Noi',
+    email: '',
+    gender: '',
+};
+
+let b = 'Eric';
+let c = '';
+console.log('What is your name ?', `My name's :`, obj.name)
+console.log('Whewe are you from ?', `I'm from :`, obj.address)
