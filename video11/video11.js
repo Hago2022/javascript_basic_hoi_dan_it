215870cedf82f4e2d5d49d20b1055130c9d41455
TongQuangHung102 <hungjob102@gmail.com>
2023-10-18 18:52:24 +0700

    for loops

---
+++ video11/video11.js
@@ -0,0 +1,13 @@
+console.log('Hello world from HTML')
+
+let arr = ['Mu', 'ManCity', 'Liver', 'Chelsea']
+for(let i = 0; i < arr.length; i++) {
+    console.log('check variable i = ', arr[i])
+}
+
+//i++ === i = i + 1 (thực hiện biến i xong mới tăng giá trị)
+//++i (tăng giá trị i rồi mới thực hiện i)
+//i++ --> assign --> increment
+//++i --> increment --> assign
+//VD: i = 1; j = ++i (i is 2, j is 2)
+//    i = 1; j = i++ (i is 2, j is 1)
