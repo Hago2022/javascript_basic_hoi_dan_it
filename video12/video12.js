047c9043c834820935e68c8df9f2973f23e169e1
TongQuangHung102 <hungjob102@gmail.com>
2023-10-20 08:29:26 +0700

    while, do while

---
+++ video12/video12.js
@@ -0,0 +1,22 @@
+console.log('Hello world from HTML');
+
+let arrTop4 = ['Man city', 'Liverpool', 'MU', 'Chelsea'];
+// for(let i = 0; i < arrTop4.length; i++) {
+//     console.log('Top: ', i + 1, arrTop4[i]);
+// }
+let i = 0;
+// while(i < arrTop4.length) {
+//     console.log('Top: ', i + 1, arrTop4[i]);
+//     i++;
+// }
+
+let flag = false;
+while (!flag) {
+    console.log('check value: ', i);
+    i++;
+    if (i === 10) flag = true;
+}
+
+// do {  
+// }
+// while();
